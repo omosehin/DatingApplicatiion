d2e3076b442ae545ec09e24a00ffdb44dfba45b2
Abayomi <omosehin14@gmail.com>
2019-12-28 23:41:01 +0100

    Add project files.

---
+++ DatingApplication/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using DatingApplication.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApplication.Data
+{
+    public class DataContext :DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            :base(options)
+        {
+                
+        }
+        public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ DatingApplication/Data/IAuthRepository.cs
@@ -0,0 +1,16 @@
+﻿using DatingApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApplication.Data
+{
+   public interface IAuthRepository
+    {
+        Task<User> Register(User user, string password);
+        Task<User> Login(string user, string password);
+        Task<bool> UserExists(string username);
+
+    }
+}
