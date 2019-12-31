3ea0b7429b569a826e551d55f797d7eb9be4a658
Abayomi <omosehin14@gmail.com>
2019-12-31 19:11:23 +0100

    Sorting and filtering

--- DatingApplication/Data/DatingRespository.cs
+++ DatingApplication/Data/DatingRespository.cs
@@ -45,7 +45,30 @@ namespace DatingApplication.Data
 
         public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
-            var users =  _context.Users.Include(p => p.Photos);
+            var users =  _context.Users.Include(p => p.Photos)
+                .OrderByDescending(u=>u.LastActive).AsQueryable();
+            users = users.Where(u => u.Id != userParams.UserId);
+            users = users.Where(u => u.Gender == userParams.Gender);
+            
+            if(userParams.MinAge !=18 || userParams.MaxAge != 99)
+            {
+                var minDob = DateTime.Today.AddYears(-userParams.MaxAge - 1);// min date of Birth
+                var maxDob = DateTime.Today.AddYears(-userParams.MinAge);// min date of Birth
+                users = users.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+            
+            }
+            if (!string.IsNullOrEmpty(userParams.OrderBy))
+            {
+                switch (userParams.OrderBy)
+                {
+                    case "created":
+                        users = users.OrderByDescending(u => u.Created);
+                        break;
+                    default:
+                        users = users.OrderByDescending(u => u.LastActive);
+                        break;
+                }
+            }
             return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
         }
 
